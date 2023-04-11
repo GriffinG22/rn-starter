923df9daf54284bdbe5d6aaa38cf2b029ae7774c
Griffin Gradke <griffin.gradke@gmail.com>
2023-04-11 13:48:36 -0500

    Section 4: Buttons, TouchableOpacity, Navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, Button } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello World</Text>
+      <Button
+        title="Go To Components Demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go To Lists Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
